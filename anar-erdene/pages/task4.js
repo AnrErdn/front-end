505b244c8912b076b589e8aba9935b04c6de4cb3
Anar-Erdene01 <g.anarerdenegantulga34@gmail.com>
2024-12-03 20:57:03 +0800

    task4 complete

--- anar-erdene/pages/task4.js
+++ anar-erdene/pages/task4.js
@@ -1,14 +1,51 @@
-import { useState } from "react";
+    import { useState } from 'react';
 
-export default function Task4() {
-    const [grid, setGrid] = useState(true);
+    //JSON data
+    const data = [
+    { id: 1, title: 'Test 1', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pulvinar hendrerit purus, at euismod diam pretium nec. Nunc gravida vulputate vulputate Nulla quis ante volutpat, condimentum ex et, congue urna.' },
+    { id: 2, title: 'Test 2', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pulvinar hendrerit purus, at euismod diam pretium nec. Nunc gravida vulputate vulputate Nulla quis ante volutpat, condimentum ex et, congue urna.' },
+    { id: 3, title: 'Test 3', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pulvinar hendrerit purus, at euismod diam pretium nec. Nunc gravida vulputate vulputate Nulla quis ante volutpat, condimentum ex et, congue urna.' },
+    { id: 4, title: 'Test 4', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pulvinar hendrerit purus, at euismod diam pretium nec. Nunc gravida vulputate vulputate Nulla quis ante volutpat, condimentum ex et, congue urna.' },
+    ];
+
+    export default function Home() {
+    const [isGridView, setIsGridView] = useState(true);
+
+    const toggleView = () => {
+        setIsGridView(!isGridView);
+    };
 
     return (
-        <div className="mx-auto max-x-4xl p-10 bg-white h-screen">
+        <div className="font-sans py-10 px-[20%] text-black bg-white h-screen">
+
             <div className="flex justify-between items-center mb-6">
-                    <h1 className="text-3xl text-black font-bold">Нийтлэлүүд</h1>
-                    <button className="text-white rounded-md bg-cyan-500 py-2 px-4" >Grid View-рүү шилжүүлэх </button>
+
+                <h1 className="text-3xl font-bold">Нийтлэлүүд</h1>
+                
+                <button 
+
+                    onClick={toggleView} 
+                    className="bg-blue-600 py-2 px-4 rounded-md shadow-md text-white hover:bg-blue-700">
+                    {isGridView ? 'Grid view-рүү шилжүүлжэх' : 'List view-рүү шилжүүлжэх'}
+
+                </button>
+
+            </div>
+
+            <div className={`grid gap-4 ${isGridView ? 'grid-cols-2' : 'grid-cols-1'}`}>
+
+                {data.map((item) => (
+                    
+                <div
+                    key={item.id}
+                    className="bg-gray-100 p-5 border-2 rounded-xl shadow-md text-left hover:bg-gray-200"
+                >
+                    <h3 className="text-2xl font-semibold mb-2">{item.title}</h3>
+                    <p className="text-md">{item.text}</p>
+                </div>
+                ))}
+
             </div>
         </div>
     );
-};
+}
