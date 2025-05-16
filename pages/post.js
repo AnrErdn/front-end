8e7cc02d3eced3a7e91764be5fd5d40695681e89
Your Name <you@example.com>
2025-05-16 13:28:38 +0800

    minor change again

--- pages/post.js
+++ pages/post.js
@@ -43,7 +43,7 @@ export default function CreateAccount() {
         }
     };
 
-    const inputClass = (hasError: boolean) =>
+    const inputClass = (hasError) =>
         `w-full border rounded-md px-3 py-2 focus:outline-none focus:ring-2 text-black ${
             hasError ? "border-red-500 focus:ring-red-500" : "border-gray-300 focus:ring-yellow-500"
         }`;
