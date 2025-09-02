787eeb9ca5d515fe90084cca1f98366108682e1d
AnrErdn <anarerdenegantulga34@gmail.com>
2025-09-02 19:23:07 +0800

    new folder created (11th grade folder)

---
+++ pages/netflix-clone/app/components/Header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface MovieType {
+  id: number;
+  title: string;
+  backdrop_path: string;
+  poster_path: string;
+}
+
+export default function Home() {
+    return (
+        <div>
+            <header className="flex justify-between items-center p-6 bg-black bg-opacity-75">
+            
+            </header>
+        </div>
+    );
+}
